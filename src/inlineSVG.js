5f1fc926f0b49c1d2b527f3d268d270d6d6383a8
João Santos <joao.santos@premium-minds.com>
2021-03-15 17:45:15 +0000

    Fix longdesc not being correctly set alongside title.

--- src/inlineSVG.js
+++ src/inlineSVG.js
@@ -1,19 +1,16 @@
 (function (root, factory) {
-
-  if(typeof define === 'function' && define.amd) {
+  if (typeof define === 'function' && define.amd) {
     define([], factory(root));
   } else if (typeof exports === 'object') {
     module.exports = factory(root);
   } else {
     root.inlineSVG = factory(root);
   }
-
-})(typeof global !== "undefined" ? global : this.window || this.global, function (root) {
-
+})(typeof global !== 'undefined' ? global : this.window || this.global, function (root) {
   // Variables
   var inlineSVG = {},
-      supports = !!document.querySelector && !!root.addEventListener,
-      settings;
+    supports = !!document.querySelector && !!root.addEventListener,
+    settings;
 
   // Defaults
   var defaults = {
@@ -27,8 +24,8 @@
    * @param {Int} times
    * @param {Function} func
    */
-  var after = function(times, func) {
-    return function() {
+  var after = function (times, func) {
+    return function () {
       if (--times < 1) {
         return func.apply(this, arguments);
       }
@@ -41,7 +38,6 @@
    * @param {Function} fn
    */
   var extend = function () {
-
     // Variables
     var extended = {};
     var deep = false;
@@ -49,18 +45,18 @@
     var length = arguments.length;
 
     // Check if a deep merge
-    if ( Object.prototype.toString.call( arguments[0] ) === '[object Boolean]' ) {
+    if (Object.prototype.toString.call(arguments[0]) === '[object Boolean]') {
       deep = arguments[0];
       i++;
     }
 
     // Merge the object into the extended object
     var merge = function (obj) {
-      for ( var prop in obj ) {
-        if ( Object.prototype.hasOwnProperty.call( obj, prop ) ) {
+      for (var prop in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, prop)) {
           // If deep merge and property is an object, merge properties
-          if ( deep && Object.prototype.toString.call(obj[prop]) === '[object Object]' ) {
-            extended[prop] = extend( true, extended[prop], obj[prop] );
+          if (deep && Object.prototype.toString.call(obj[prop]) === '[object Object]') {
+            extended[prop] = extend(true, extended[prop], obj[prop]);
           } else {
             extended[prop] = obj[prop];
           }
@@ -69,13 +65,12 @@
     };
 
     // Loop through each object and conduct a merge
-    for ( ; i < length; i++ ) {
+    for (; i < length; i++) {
       var obj = arguments[i];
       merge(obj);
     }
 
     return extended;
-
   };
 
   // Methods
@@ -85,10 +80,8 @@
    * @public
    */
   var getAll = function () {
-
     var svgs = document.querySelectorAll(settings.svgSelector);
     return svgs;
-
   };
 
   var uid = function () {
@@ -103,29 +96,25 @@
    * @public
    */
   var inliner = function (cb) {
-
     var svgs = getAll();
     var callback = after(svgs.length, cb);
 
     Array.prototype.forEach.call(svgs, function (svg, i) {
-
       // Store some attributes of the image
       var src = svg.src || svg.getAttribute('data-src'),
-          attributes = svg.attributes;
+        attributes = svg.attributes;
 
       // Get the contents of the SVG
       var request = new XMLHttpRequest();
       request.open('GET', src, true);
 
       request.onload = function () {
-
-        if(request.status >= 200 && request.status < 400) {
-
+        if (request.status >= 200 && request.status < 400) {
           // Setup a parser to convert the response to text/xml in order for it
           // to be manipulated and changed
           var parser = new DOMParser(),
-              result = parser.parseFromString(request.responseText, 'text/xml'),
-              inlinedSVG = result.getElementsByTagName('svg')[0];
+            result = parser.parseFromString(request.responseText, 'text/xml'),
+            inlinedSVG = result.getElementsByTagName('svg')[0];
 
           // Remove some of the attributes that aren't needed
           inlinedSVG.removeAttribute('xmlns:a');
@@ -138,10 +127,14 @@
           inlinedSVG.removeAttribute('xml:space');
           inlinedSVG.removeAttribute('version');
 
-          // Add in the attributes from the original <img> except `src` or
-          // `alt`, we don't need either
-          Array.prototype.slice.call(attributes).forEach(function(attribute) {
-            if(attribute.name !== 'src' && attribute.name !== 'alt') {
+          // Add in the attributes from the original <img> except `src`,
+          // `alt` or `longdesc` which we don't need
+          Array.prototype.slice.call(attributes).forEach(function (attribute) {
+            if (
+              attribute.name !== 'src' &&
+              attribute.name !== 'alt' &&
+              attribute.name !== 'longdesc'
+            ) {
               inlinedSVG.setAttribute(attribute.name, attribute.value);
             }
           });
@@ -151,59 +144,70 @@
           if (inlinedSVG.classList) {
             inlinedSVG.classList.add('inlined-svg');
           } else {
-            inlinedSVG.className += ' ' + 'inlined-svg';
+            inlinedSVG.setAttribute('class', inlinedSVG.getAttribute('class') + ' inlined-svg');
           }
 
           // Add in some accessibility quick wins
           inlinedSVG.setAttribute('role', 'img');
 
-          // Use the `longdesc` attribute if one exists
-          if(attributes.longdesc) {
-            var description = document.createElementNS('http://www.w3.org/2000/svg', 'desc'),
-                descriptionText = document.createTextNode(attributes.longdesc.value);
-
-            description.setAttribute('id', uid());
-            description.appendChild(descriptionText);
-            inlinedSVG.insertBefore(description, inlinedSVG.firstChild);
-          }
-
           // Use the `alt` attribute if one exists
-          if(attributes.alt) {
-            var title = document.createElementNS('http://www.w3.org/2000/svg', 'title'),
-                titleText = document.createTextNode(attributes.alt.value);
+          if (attributes.alt) {
+            let title = document.createElementNS('http://www.w3.org/2000/svg', 'title'),
+              titleText = document.createTextNode(attributes.alt.value);
 
             title.setAttribute('id', uid());
             title.appendChild(titleText);
             inlinedSVG.insertBefore(title, inlinedSVG.firstChild);
 
-            if(attributes.id) {
+            if (attributes.id) {
               inlinedSVG.setAttribute('aria-labelledby', attributes.id.value);
             } else if (!attributes.id) {
-              var getTitleId = function () {
+              let getTitleId = function () {
                 if (inlinedSVG.getElementsByTagName('title').length > 0) {
-                  var titleId = inlinedSVG.getElementsByTagName('title')[0].getAttribute('id');
+                  let titleId = inlinedSVG.getElementsByTagName('title')[0].getAttribute('id');
                   return titleId;
                 } else {
                   return '';
                 }
               };
 
-              var getDescId = function () {
-                if (inlinedSVG.getElementsByTagName('desc').length > 0) {
-                  var descId = inlinedSVG.getElementsByTagName('desc')[0].getAttribute('id');
-                  return descId;
-                } else {
-                  return '';
-                }
-              };
-
-              inlinedSVG.setAttribute('aria-labelledby', getTitleId() + ' ' + getDescId());
+              inlinedSVG.setAttribute('aria-labelledby', getTitleId());
             }
           }
 
-          if(!attributes.alt) {
+          if (!attributes.alt) {
             inlinedSVG.setAttribute('aria-hidden', 'true');
             inlinedSVG.setAttribute('role', 'presentation');
+          }
+
+          // Use the `longdesc` attribute if one exists
+          if (attributes.longdesc) {
+            let description = document.createElementNS('http://www.w3.org/2000/svg', 'desc'),
+              descriptionText = document.createTextNode(attributes.longdesc.value);
+
+            description.setAttribute('id', uid());
+            description.appendChild(descriptionText);
+            if (attributes.alt) {
+              inlinedSVG.insertBefore(description, inlinedSVG.firstChild.nextSibling);
+            } else {
+              inlinedSVG.insertBefore(description, inlinedSVG.firstChild);
+            }
+
+            let getDescId = function () {
+              if (inlinedSVG.getElementsByTagName('desc').length > 0) {
+                let descId = inlinedSVG.getElementsByTagName('desc')[0].getAttribute('id');
+                return descId;
+              } else {
+                return '';
+              }
+            };
+
+            if (attributes.alt) {
+              let currAttrs = inlinedSVG.getAttribute('aria-labelledby');
+              inlinedSVG.setAttribute('aria-labelledby', (currAttrs += ' ' + getDescId()));
+            } else {
+              inlinedSVG.setAttribute('aria-labelledby', getDescId());
+            }
           }
 
           // Replace the image with the SVG
@@ -215,13 +219,9 @@
           if (callback) {
             callback(settings.svgSelector);
           }
-
         } else {
-
           console.error('There was an error retrieving the source of the SVG.');
-
         }
-
       };
 
       request.onerror = function () {
@@ -229,9 +229,7 @@
       };
 
       request.send();
-
     });
-
   };
 
   /**
@@ -239,7 +237,6 @@
    * @public
    */
   inlineSVG.init = function (options, callback) {
-
     // Test for support
     if (!supports) return;
 
@@ -247,13 +244,11 @@
     settings = extend(defaults, options || {});
 
     // Kick-off the inliner
-    inliner(callback || function(){});
+    inliner(callback || function () {});
 
     // Once inlined and a class to the HTML
     document.documentElement.className += ' ' + settings.initClass;
-
   };
 
   return inlineSVG;
-
 });
